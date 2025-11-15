da17ee8463f5642e9310a3947e96e79b2d5396ee
Huynh Trung Hieu <kjhieu14@gmail.com>
2025-11-15 08:30:34 +0700

    init project

---
+++ apps/be/src/main/java/com/dattcn2526nhom14/quanlyluutru/QuanlyluutruApplication.java
@@ -0,0 +1,13 @@
+package com.dattcn2526nhom14.quanlyluutru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyluutruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyluutruApplication.class, args);
+	}
+
+}
---
+++ apps/be/src/test/java/com/dattcn2526nhom14/quanlyluutru/QuanlyluutruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dattcn2526nhom14.quanlyluutru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyluutruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
